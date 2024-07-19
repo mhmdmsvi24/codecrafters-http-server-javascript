cc34e95c2476717c0012d9355338b53aa2d828f2
mhmdmsvi24 <mousavimohammadg@gmail.com>
2024-07-19 18:01:48 +0330

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,20 +1,41 @@
 const net = require("net");
 
-// Creates a TCP client/server model server
 const server = net.createServer((socket) => {
-  // adds an event handler (the callback) for the specified event (data here)
+  console.log("Client connected");
+
   socket.on("data", (data) => {
-    const request = data.toString();
-    const path = request.split(" ")[1];
+    const requestString = data.toString();
+    console.log("Request received:\n", requestString);
 
-    const response = path === "/" ? "200 OK" : "404 Not Found";
+    // Parse the request
+    const [requestLine] = requestString.split("\r\n");
+    const url = requestLine.split(" ")[1];
 
-    socket.write(`HTTP/1.1 ${response}\r\n\r\n`);
+    // Check if it's a GET request to /echo/
+    if (url == "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (url.includes("/echo/")) {
+      const content = url.split("/echo/")[1];
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
+      );
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
 
-    socket.end();
+    socket.end(); // Close the connection
+  });
+
+  socket.on("error", (err) => {
+    console.error("Socket error:", err);
+  });
+
+  socket.on("end", () => {
+    console.log("Client disconnected");
   });
 });
 
-server.listen(4221, "localhost", () => {
-  process.stdout.write("Listening on localhost:4221\n");
+const PORT = 4221;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
