2e9027d890293db70d8d022bf88e12bbde59f079
mhmdmsvi24 <mousavimohammadg@gmail.com>
2024-07-19 19:06:41 +0330

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,48 +1,41 @@
+const fs = require("fs");
 const net = require("net");
-
+console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
-  console.log("Client connected");
-
-  socket.on("data", (data) => {
-    const requestString = data.toString();
-    const [requestLine, ...headers] = requestString.split("\r\n");
-    const [method, url] = requestLine.split(" ");
-
-    // endpoint /
-    if (url === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-      // endpoint /echo/
-    } else if (url.startsWith("/echo/")) {
-      const content = url.slice(6); // Remove '/echo/' from the beginning
-      socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
-      );
-      // endpoint /user-agent
-    } else if (url === "/user-agent") {
-      const userAgent = headers.find((h) =>
-        h.toLowerCase().startsWith("user-agent:")
-      );
-      const content = userAgent ? userAgent.split(": ")[1] : "";
-      socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
-      );
-    } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-    }
-
+  socket.on("close", () => {
     socket.end();
   });
-
-  socket.on("error", (err) => {
-    console.error("Socket error:", err);
-  });
-
-  socket.on("end", () => {
-    console.log("Client disconnected");
+  socket.on("data", (data) => {
+    const req = data.toString();
+    console.log(req);
+    const path = req.split(" ")[1];
+    if (path === "/") socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    else if (path.startsWith("/files/")) {
+      const directory = process.argv[3];
+      const filename = path.split("/files/")[1];
+      if (fs.existsSync(`${directory}/${filename}`)) {
+        const content = fs.readFileSync(`${directory}/${filename}`).toString();
+        const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`;
+        socket.write(res);
+      } else {
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      }
+    } else if (path === "/user-agent") {
+      req.split("\r\n").forEach((line) => {
+        if (line.includes("User-Agent")) {
+          const res = line.split(" ")[1];
+          socket.write(
+            `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${res.length}\r\n\r\n${res}\r\n`
+          );
+        }
+      });
+    } else if (path.startsWith("/echo/")) {
+      const res = path.split("/echo/")[1];
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${res.length}\r\n\r\n${res}\r\n\r`
+      );
+    } else socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    socket.end();
   });
 });
-
-const PORT = 4221;
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+server.listen(4221, "localhost");
