b1c9069790087f26e6e858ef3c9924874022e21c
mhmdmsvi24 <mousavimohammadg@gmail.com>
2024-07-19 17:24:49 +0330

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,14 +1,20 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this to pass the first stage
+// Creates a TCP client/server model server
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  socket.on("close", () => {
+  // adds an event handler (the callback) for the specified event (data here)
+  socket.on("data", (data) => {
+    const request = data.toString();
+    const path = request.split(" ")[1];
+
+    const response = path === "/" ? "200 OK" : "404 Not Found";
+
+    socket.write(`HTTP/1.1 ${response}\r\n\r\n`);
+
     socket.end();
   });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, "localhost", () => {
+  process.stdout.write("Listening on localhost:4221\n");
+});
